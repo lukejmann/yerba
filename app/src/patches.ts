566c54ef93a1d09e463f3e3e0713d5bbccbab5d0
lukejmann <luke@mann.xyz>
2023-06-22 04:02:32 -0700

    fix: wss

--- app/src/patches.ts
+++ app/src/patches.ts
@@ -2,11 +2,12 @@ import { wsBatchLink } from '@rspc/client/v2';
 
 const serverOrigin = import.meta.env.DEV ? 'localhost:8080' : 'yerba-dev.fly.dev';
 const http = import.meta.env.DEV ? 'http' : 'https';
+const ws = import.meta.env.DEV ? 'ws' : 'wss';
 
 globalThis.isDev = import.meta.env.DEV;
 globalThis.rspcLinks = [
 	wsBatchLink({
-		url: `ws://${serverOrigin}/rspc/ws`
+		url: `${ws}://${serverOrigin}/rspc/ws`
 	})
 ];
 globalThis.serverOrigin = serverOrigin;
